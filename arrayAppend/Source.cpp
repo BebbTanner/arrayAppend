68a1144ac67395d0bbd74534da9498752dbfc5b8
Tanner Bebb <17tbebb@gmail.com>
2024-02-07 08:05:17 -0800

    Setup my todo list for this program. Added some notes at the top of the program. Imported the vector library. Initialized the vector called "myVector'. Created a cout statement that ask the user to enter some numbers. Declared the variable breakOut. Inital setup for my while loop.

--- arrayAppend/Source.cpp
+++ arrayAppend/Source.cpp
@@ -1,6 +1,30 @@
+/*Initially I wanted to create a program that allows the user to add and remove values from an array.
+After looking into it, it would seem that I cannot do that. So, instead I will be using a vector to that 
+instead.
+
+TODO:
+	Initialize a vector
+	Ask the user to input the values in the vector
+	Create a do-while loop that will allow users to finish inputing values
+	Add something that will allow users to remove items from the vector
+	Add a for-loop to print out the values stored in the vector
+
+	*/
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
+	vector <int> myVector;
 
+	cout << "Please enter some numbers, Press Q to quit: " << endl;
+
+	char breakOut;
+
+	while (breakOut != 'Q' || breakOut != 'q') {
+
+	}
+
+	return 0;
 }
